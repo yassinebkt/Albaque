02dacdb08dfbe8bda1093d13c38f9a7069c36bd0
Yassine <yassine753357@gmail.com>
2017-04-20 12:31:06 +0200

    Add PorjetClient View Midel

---
+++ Albaque/Albaque/ViewModels/ProjetClientViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Albaque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Albaque2.ViewModels
+{
+    public class ProjetClientViewModel
+    {
+        public IEnumerable<Client> clients { get; set; }
+        public Projet projet { get; set; }       
+    }
+}
